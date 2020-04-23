b06a633327787e27cc430876e86281b67fda65a2
Renato <renato.farruggio@stud.unibas.ch>
2020-04-23 17:34:44 +0200

    Implement simple qr code reader. If camera does not work, turn it on in phone permissions.

--- app/src/main/java/ch/unibas/qrscanner/MainActivity.java
+++ app/src/main/java/ch/unibas/qrscanner/MainActivity.java
@@ -2,13 +2,32 @@ package ch.unibas.qrscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static TextView resultTextView;
+    Button scanButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultTextView = (TextView)findViewById(R.id.result_text);
+        scanButton = (Button) findViewById(R.id.btn_scan);
+
+        scanButton.setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
+            }
+        });
     }
 }
