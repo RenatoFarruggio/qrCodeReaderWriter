f1d0344007b97001e19a7723623ef4ef5ae9df5a
Renato <renato.farruggio@stud.unibas.ch>
2020-04-23 18:34:59 +0200

    Can now show the written qr code, decoded and encoded again.

--- app/src/main/java/ch/unibas/qrscanner/MainActivity.java
+++ app/src/main/java/ch/unibas/qrscanner/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import ch.unibas.qrscanner.intenthelper.IntentIntegrator;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static TextView resultTextView;
     Button scanButton;
+    public static ImageView qrImageView;
 
 
     @Override
@@ -23,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         resultTextView = (TextView)findViewById(R.id.result_text);
         scanButton = (Button) findViewById(R.id.btn_scan);
+        qrImageView = (ImageView) findViewById(R.id.result_qr_code);
 
         scanButton.setOnClickListener(new View.OnClickListener()  {
             @Override
@@ -31,12 +34,14 @@ public class MainActivity extends AppCompatActivity {
                 // Using back camera
                 startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
 
-                // Using front camera   // to be continued....
+                /* to be continued....
+                // Using front camera
                 // if we do not use this, remove the intenthelper package!!
-                //IntentIntegrator integrator = new IntentIntegrator();
-                //integrator.setCameraId(1);
-                //integrator.initiateScan();
-                //startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class).putExtra("SCAN_CAMERA_ID", 1));
+                IntentIntegrator integrator = new IntentIntegrator();
+                integrator.setCameraId(1);
+                integrator.initiateScan();
+                startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class).putExtra("SCAN_CAMERA_ID", 1));
+                 */
             }
         });
     }
--- app/src/main/java/ch/unibas/qrscanner/ScanCodeActivity.java
+++ app/src/main/java/ch/unibas/qrscanner/ScanCodeActivity.java
@@ -2,11 +2,19 @@ package ch.unibas.qrscanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.AndroidException;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.Result;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
@@ -24,6 +32,22 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
     public void handleResult(Result result) {
 
         MainActivity.resultTextView.setText(result.getText());
+
+
+        // Initially copied from:
+        // https://medium.com/@aanandshekharroy/generate-barcode-in-android-app-using-zxing-64c076a5d83a
+
+        String text=result.getText();
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(text, BarcodeFormat.QR_CODE,200,200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            MainActivity.qrImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+
         onBackPressed();
     }
 
