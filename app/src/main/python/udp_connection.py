86091d113391f678f8f03668e6901fbb8bb7c0e7
Renato <renato.farruggio@stud.unibas.ch>
2020-05-21 10:46:14 +0200

    Update extern libraries from logSync

--- app/src/main/python/udp_connection.py
+++ app/src/main/python/udp_connection.py
@@ -3,32 +3,39 @@ import cbor
 import main
 import pcap
 import sync
+import time
 
 buffSize = 2048
 
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, port):
         port = int(port)
 
         # Create the socket and bind to host and port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((host, port))  # 1
-
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socket.setblocking(False)
         self.__package_to_send_as_bytes = []
 
-        print("Server started: <" + host + ">:<" + str(port) + ">")
-        print('Waiting for query...')
+        print("Server started broadcasting at port" + str(port))
+        connected = True
+        while connected:
+            self.socket.sendto(b'broadcasting_looking_for_other_device', ('<broadcast>', port))
+            time.sleep(3)
+            try:
+                # Server receives information about the request
+                info_request, address = self.socket.recvfrom(buffSize)  # 4
+                if info_request == str.encode('requesting_infos_of_all_pcap_files'):
+                    list_of_files = main.create_list_of_files('udpDir/')  # 4
+                    self.socket.sendto(cbor.dumps(list_of_files), address)  # 4
+                    print("Request accepted, list of files sent.")
+                    connected = False
+            except:
+                print('Waiting for request...')
+        self.socket.setblocking(True)
 
-        # Server receives information about the request
-        info_request, address = self.socket.recvfrom(buffSize)  # 4
-        if info_request == str.encode('requesting_infos_of_all_pcap_files'):
-            list_of_files = main.create_list_of_files('udpDir/')  # 4
-            self.socket.sendto(cbor.dumps(list_of_files), address)  # 4
-            print("Request accepted, list of files sent.")
-        else:
-            print("Request denied.")
-            return
         # Server receives the list with the necessary log extensions
         list_with_necessary_files = cbor.loads(self.socket.recv(buffSize))  # 9
 
@@ -58,24 +65,35 @@ class Server:
 
 
 class Client:
-    def __init__(self, host, port):
-        address = (host, int(port))
+    def __init__(self, port):
 
         # Create the UDP socket and request the log extensions
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # 2
-        self.socket.sendto(str.encode('requesting_infos_of_all_pcap_files'), address)  # 3
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socket.bind(('', int(port)))
+
+        broadcasting = True
+        while broadcasting:
+            print("Waiting for broadcaster...")
+            msg, address = self.socket.recvfrom(buffSize)
+            if msg == b'broadcasting_looking_for_other_device':
+                print("Requesting a list the information about the files and their sequence number...")
+                self.socket.sendto(str.encode('requesting_infos_of_all_pcap_files'), address)  # 3
+                broadcasting = False
 
         self.__received_package_as_events = []
 
         # Contains a list of all files of the server side and their sequence number
         requested_list = cbor.loads(self.socket.recv(buffSize))  # 5 & 6
+        print("List received...")
 
-        self.__compared_files = sync.compare_files(requested_list)  # 7
-        self.socket.sendto(cbor.dumps(self.__compared_files), address)  # 8
-
+        self.__list_of_needed_extensions = sync.compare_files(requested_list)  # 7
+        self.socket.sendto(cbor.dumps(self.__list_of_needed_extensions), address)  # 8
+        print("Files compared and sending information about the needed extensions...")
         #############################################################################################
         # Client receives log extensions one by one and appends them
-        for file_info in self.__compared_files:
+        for file_info in self.__list_of_needed_extensions:
             event = self.socket.recv(buffSize)  # 11
             self.__received_package_as_events.append(event)
 
@@ -89,5 +107,5 @@ class Client:
     def get_packet_to_receive_as_bytes(self):
         return self.__received_package_as_events
 
-    def get_compared_files(self):
-        return self.__compared_files
+    def get_list_of_needed_extensions(self):
+        return self.__list_of_needed_extensions
